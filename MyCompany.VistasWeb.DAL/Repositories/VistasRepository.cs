faf5fad87140f32a5c239e98cc8f1d33da24a899
brand <brand@FUJITPC>
2023-12-02 17:18:41 -0400

    se agrega el proyecto

---
+++ MyCompany.VistasWeb.DAL/Repositories/VistasRepository.cs
@@ -0,0 +1,54 @@
+﻿using MyCompany.VistasWeb.DAL.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCompany.VistasWeb.Models;
+using MyCompany.VistasWeb.DAL.DataContext;
+
+namespace MyCompany.VistasWeb.DAL.Repositories
+{
+    public class VistasRepository : IGenericRepository<Cliente>
+    {
+
+        private readonly GestionPedidosContext _dbContext;
+
+        public VistasRepository(GestionPedidosContext context)
+        {
+            _dbContext=context;
+        }
+        public async Task<bool> Actualizar(Cliente modelo)
+        {
+          _dbContext.Clientes.Update(modelo);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            Cliente modelo = _dbContext.Clientes.First(c => c.Id == id);
+            _dbContext.Clientes.Remove(modelo);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Insertar(Cliente modelo)
+        {
+           _dbContext.Clientes.Add(modelo);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Cliente> Obtener(int id)
+        {
+            return await _dbContext.Clientes.FindAsync(id);
+        }
+
+        public async Task<IQueryable<Cliente>> ObtenerTodos()
+        {
+            IQueryable<Cliente> queryClienteSql = _dbContext.Clientes;
+            return queryClienteSql;
+        }
+    }
+}
---
+++ MyCompany.VistasWeb.UI/Controllers/ClienteController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.VistasWeb.Logic.Contracts;
+using MyCompany.VistasWeb.Models;
+using MyCompany.VistasWeb.UI.Models;
+using MyCompany.VistasWeb.UI.Models.ViewModels;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace MyCompany.VistasWeb.UI.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly IClienteServices _clienteServices;
+
+        public ClienteController(IClienteServices clienteServices)
+        {
+            _clienteServices = clienteServices;
+        }
+        public async Task<IActionResult> Index(ClienteViewModel clienteViewModel)
+        {
+            IQueryable<Cliente> queryClienteSQL = await _clienteServices.ObtenerTodos();
+
+            List<ClienteViewModel> LstclienteViewModel = queryClienteSQL
+                                                      .Select(c => new ClienteViewModel()
+                                                      {
+                                                          Id = c.Id,
+                                                          Nombre = c.Nombre,
+                                                          Razon = c.Razon,
+                                                          Rif = c.Rif,
+                                                          Direccion = c.Direccion,
+                                                          Correo = c.Correo,
+                                                          Telefono = c.Telefono,
+                                                          //Activo = c.Activo.To,
+                                                          Notas = c.Notas
+                                                      }).ToList();
+            return View(LstclienteViewModel);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Create(ClienteViewModel clienteViewModel)
+        {
+            Cliente cliente = new Cliente()
+            {
+                //Id= clienteViewModel.Id,
+                Nombre = clienteViewModel.Nombre,
+                Razon = clienteViewModel.Razon,
+                Rif = clienteViewModel.Rif,
+                Direccion = clienteViewModel.Direccion,
+                Correo = clienteViewModel.Correo,
+                Telefono = clienteViewModel.Telefono,
+                Activo = clienteViewModel.Activo,
+                Notas = clienteViewModel.Notas
+            };
+
+            if(ModelState.IsValid)
+            {
+                bool respuesta=await _clienteServices.Insertar(cliente);
+                return RedirectToAction("Index", "Cliente");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult>Edit(int id)
+        {
+            Task<Cliente> _cliente = _clienteServices.Obtener(id);
+
+            ClienteViewModel clienteViewModel = new ClienteViewModel()
+            {
+                Id= _cliente.Result.Id,
+                Nombre=_cliente.Result.Nombre,
+                Razon=_cliente.Result.Razon,
+                Rif= _cliente.Result.Rif,
+                Direccion=_cliente.Result.Direccion,
+                Correo= _cliente.Result.Correo,
+                Telefono= _cliente.Result.Telefono,
+                //Activo=_cliente.Result.Activo,
+                Notas= _cliente.Result.Notas
+            };
+            return View(clienteViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Edit(ClienteViewModel clienteViewModel)
+        {
+            Cliente cliente = new Cliente()
+            {
+                Id = clienteViewModel.Id,
+                Nombre = clienteViewModel.Nombre,
+                Razon = clienteViewModel.Razon,
+                Rif = clienteViewModel.Rif,
+                Direccion= clienteViewModel.Direccion,
+                Correo= clienteViewModel.Correo,
+                Telefono = clienteViewModel.Telefono,
+                //Activo= clienteViewModel.Activo,
+                Notas= clienteViewModel.Notas
+            };
+            if(ModelState.IsValid)
+            {
+                bool respuesta=await _clienteServices.Actualizar(cliente);
+                return RedirectToAction("Index","Cliente");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Task<Cliente> _cliente = _clienteServices.Obtener(id);
+
+            ClienteViewModel clienteViewModel = new ClienteViewModel()
+            {
+                Id = _cliente.Result.Id,
+                Nombre = _cliente.Result.Nombre,
+                Razon = _cliente.Result.Razon,
+                Rif = _cliente.Result.Rif,
+                Direccion = _cliente.Result.Direccion,
+                Correo = _cliente.Result.Correo,
+                Telefono = _cliente.Result.Telefono,
+                //Activo = _cliente.Result.Activo,
+                Notas = _cliente.Result.Notas
+            };
+            return View(clienteViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id, ClienteViewModel clienteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                bool respuesta=await _clienteServices.Eliminar(id);
+                return RedirectToAction("Index", "Cliente");
+            }
+
+            return View();
+        }
+    }
+}
