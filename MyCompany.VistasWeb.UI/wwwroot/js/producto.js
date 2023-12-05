63f49e8c584a3ce96c308c488bf54ea075b3a945
BrandonPelaez <150721120+BrandonPelaez@users.noreply.github.com>
2023-12-04 20:24:07 -0400

    Add files via upload

---
+++ MyCompany.VistasWeb.UI/wwwroot/js/producto.js
@@ -0,0 +1,22 @@
+const store = document.getElementById('store');
+const barraLateral = document.querySelector('.barra-lateral');
+const spans = document.querySelectorAll("span");
+const menu = document.querySelector('.menu');
+const main = document.querySelector('main');
+menu.addEventListener("click",()=>{
+  barraLateral.classList.toggle('max-barra-lateral')
+  if (barraLateral.classList.contains('max-barra-lateral')) {
+     menu.children[0].style.display = 'none'
+     menu.children[1].style.display = 'block'
+  }else{
+    menu.children[0].style.display = 'block'
+     menu.children[1].style.display = 'none'
+  }
+})
+store.addEventListener("click",()=>{
+  barraLateral.classList.toggle("mini-barra-lateral");
+  main.classList.toggle("min-main")
+  spans.forEach((span)=>{
+    span.classList.toggle("oculto");
+  })
+})
