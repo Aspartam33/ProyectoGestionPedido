5cb2801830d74b5f84fa911db9600ff00229837b
aspartam33 <chaparrodavid91@gmail.com>
2023-12-07 21:13:49 -0400

    validacion modelos

--- ProyectoLourtec2023.GestionPedido.UI/Models/ViewModels/VendedorViewModel.cs
+++ ProyectoLourtec2023.GestionPedido.UI/Models/ViewModels/VendedorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ProyectoLourtec2023.GestionPedido.DAL.DataContext;
 using ProyectoLourtec2023.GestionPedido.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoLourtec2023.GestionPedido.UI.Models.ViewModels
 {
@@ -7,16 +8,17 @@ namespace ProyectoLourtec2023.GestionPedido.UI.Models.ViewModels
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Ingresa el nombre del usuario")]
         public string? Nombre { get; set; }
 
         public string? Razon { get; set; }
-
+        [Required(ErrorMessage = "Ingresa el número de cédula")]
         public string? Rif { get; set; }
-
+        [Required(ErrorMessage = "Ingresa la dirección del usuario")]
         public string? Direccion { get; set; }
-
+        [Required(ErrorMessage = "Ingresa el correo electrónico")]
         public string? Correo { get; set; }
-
+        [Required(ErrorMessage = "Ingresa el campo teléfono está vacío")]
         public string? Telefono { get; set; }
 
         public bool Activo { get; set; }
