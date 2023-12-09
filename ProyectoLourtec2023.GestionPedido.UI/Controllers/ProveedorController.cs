f47ad14de07283fe3968c049d850540e49e8feed
aspartam33 <chaparrodavid91@gmail.com>
2023-12-08 21:20:11 -0400

    integracion proveedores

---
+++ ProyectoLourtec2023.GestionPedido.UI/Controllers/ProveedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoLourtec2023.GestionPedido.UI.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoLourtec2023.GestionPedido.UI/Models/ViewModels/ProveedorViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoLourtec2023.GestionPedido.UI.Models.ViewModels
+{
+    public class ProveedorViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+        public string? RazonSocial { get; set;}
+        public string? Rif { get; set;}
+        public string? Direccion { get; set;}
+
+        public string? Telefonos { get; set;}
+
+        public string? Correos { get; set;}
+        public string? Contacto { get; set;}
+
+        public int IdTipoProv {  get; set;}
+
+        public bool Activo { get; set; }
+
+
+    }
+}
